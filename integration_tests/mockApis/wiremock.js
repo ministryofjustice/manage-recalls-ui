fc981e9e9a95c8fa422ef1a28126b54f6895bb8d
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-11 08:57:50 +0100

    Keep lint happy, remove console statement

--- integration_tests/mockApis/wiremock.js
+++ integration_tests/mockApis/wiremock.js
@@ -2,7 +2,6 @@ import superagent from 'superagent'
 
 export default function wiremock(wiremockUrl) {
   const wiremockAdminUrl = `${wiremockUrl}/__admin`
-  console.log(`wiremockAdminUrl: ${wiremockAdminUrl}`)
   const stubFor = mapping => superagent.post(`${wiremockAdminUrl}/mappings`).send(mapping)
   const getRequests = () => superagent.get(`${wiremockAdminUrl}/requests`)
   const resetStubs = () =>
