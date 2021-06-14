6d41fd995b92165000bd9c363ea8187977c863c3
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-14 12:02:40 +0100

    Allow setting up specific expectations in the manageRecallsApis.  Add more assertions when searching for an offender

--- integration_tests/mockApis/manageRecallsApi.js
+++ integration_tests/mockApis/manageRecallsApi.js
@@ -1,24 +1,24 @@
 export default function manageRecallsApi(wiremock) {
   return {
-    stubOffenderSearch: () => {
+    expectSearchResults: expectation => {
       return wiremock.stubFor({
         request: {
           method: 'POST',
           urlPattern: '/search',
+          bodyPatterns: [
+            {
+              equalToJson: {
+                name: expectation.expectedSearchTerm,
+              },
+            },
+          ],
         },
         response: {
           status: 200,
           headers: {
             'Content-Type': 'application/json;charset=UTF-8',
           },
-          jsonBody: [
-            {
-              firstName: 'Bobby',
-              lastName: 'Badger',
-              nomisNumber: '123ABC',
-              dateOfBirth: '1999-05-28',
-            },
-          ],
+          jsonBody: expectation.expectedSearchResults,
         },
       })
     },
