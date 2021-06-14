bd295ebdb25a274c217008bbdca64490fcbc4eff
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-14 10:42:07 +0100

    Add start-recall spec.

--- integration_tests/pages/index.js
+++ integration_tests/pages/index.js
@@ -3,6 +3,7 @@ const page = require('./page')
 const indexPage = () =>
   page('Manage a recall for an offender on licence', {
     headerUserName: () => cy.get('[data-qa=header-user-name]'),
+    startRecall: () => cy.get('[data-qa=start-recall]').click(),
   })
 
 module.exports = { verifyOnPage: indexPage }
