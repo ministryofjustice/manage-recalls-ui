c75ea43995d983ddc47f608d126a3baf0ffcebd0
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-24 16:15:09 +0100

    PUD-78:  Update script to start services for integration tests

--- build.sh
+++ build.sh
@@ -2,23 +2,4 @@
 
 set -euo pipefail
 
-readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-function checkCypressInstalled() {
-  local cypressBinaryString==`${SCRIPT_DIR}/node_modules/.bin/cypress version | grep "Cypress binary version:"`
-  local cypressVersion=${cypressBinaryString:25}
-
-  if [[ $cypressVersion == 'not installed' ]]; then
-      echo "Cypress not installed"
-      npm install --save-dev cypress
-  else
-      echo "Found Cypress binary version ${cypressVersion}"
-  fi
-}
-
-function runBuild() {
-  npm run local:build
-}
-
-checkCypressInstalled
-runBuild
+npm run local:build
