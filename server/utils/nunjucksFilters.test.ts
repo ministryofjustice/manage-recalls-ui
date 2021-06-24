f5c861c89ba3defc3d219ee71d988ef931448914
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-24 08:35:10 +0100

    PUD-18:  Update 'n people found' text to cater for '1 person found'.  Don't show result headers if no people found

---
+++ server/utils/nunjucksFilters.test.ts
@@ -0,0 +1,33 @@
+import { personOrPeopleFilter, userNameFilter } from './nunjucksFilters'
+
+describe('personOrPeopleFilter', () => {
+  it('count is 0', () => {
+    expect(personOrPeopleFilter(0)).toEqual('0 people')
+  })
+
+  it('count is 1', () => {
+    expect(personOrPeopleFilter(1)).toEqual('1 person')
+  })
+
+  it('count is 2', () => {
+    expect(personOrPeopleFilter(2)).toEqual('2 people')
+  })
+})
+
+describe('userNameFilter', () => {
+  it('undefined name returns null', () => {
+    expect(userNameFilter(undefined)).toEqual(null)
+  })
+
+  it('single name returns itself', () => {
+    expect(userNameFilter('Smith')).toEqual('Smith')
+  })
+
+  it('two names returns first name capitalised and last name', () => {
+    expect(userNameFilter('John Smith')).toEqual('J. Smith')
+  })
+
+  it('three names returns first name capitalised and last name', () => {
+    expect(userNameFilter('John Charles Smith')).toEqual('J. Smith')
+  })
+})
---
+++ server/utils/nunjucksFilters.ts
@@ -0,0 +1,17 @@
+export function personOrPeopleFilter(count: number): string {
+  if (count === 1) {
+    return '1 person'
+  }
+  return `${count} people`
+}
+
+export function userNameFilter(fullName: string): string {
+  if (!fullName) {
+    return null
+  }
+  const nameParts = fullName.split(' ')
+  if (nameParts.length === 1) {
+    return fullName
+  }
+  return `${nameParts[0][0]}. ${nameParts.reverse()[0]}`
+}
