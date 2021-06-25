879ab0fc66aae3879496847c1113d974f39be38a
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-06-25 12:52:34 +0100

    PUD-78: [MD NO] Retrieve revocation order from manageRecallsApi.

--- integration_tests/integration/generate-revocation-order.spec.js
+++ integration_tests/integration/generate-revocation-order.spec.js
@@ -1,8 +1,12 @@
+import validateBinaryFile from './file-utils'
+
 const IndexPage = require('../pages/index')
 
 context('Generate revocation order', () => {
   beforeEach(() => {
-    cy.task('resetAndStubLogin')
+    cy.task('reset')
+    cy.task('stubLogin')
+    cy.task('stubAuthUser')
   })
 
   const nomsNumber = '123ABC'
@@ -16,17 +20,31 @@ context('Generate revocation order', () => {
       },
     ])
 
-    cy.login()
-    const homePage = IndexPage.verifyOnPage()
-    homePage.searchFor(nomsNumber)
-    homePage.expectSearchResultsCountText('1 person found')
-    homePage.searchResults().find('tr').should('have.length', 1)
-    const firstResult = homePage.searchResults().first()
-    firstResult.get('[data-qa=generateRevocationOrderButton]').click()
-    firstResult.get('[data-qa=data]')
+    cy.readFile('integration_tests/expected-revocation-order.pdf', 'base64').then(base64EncodedPdf => {
+      expectRevocationOrderFromManageRecallsApi({ contents: base64EncodedPdf })
+      cy.login()
+
+      const homePage = IndexPage.verifyOnPage()
+      homePage.searchFor(nomsNumber)
+      homePage.expectSearchResultsCountText('1 person found')
+      homePage.searchResults().find('tr').should('have.length', 1)
+      const firstResult = homePage.searchResults().first()
+      firstResult.get('[data-qa=generateRevocationOrderButton]').click()
+
+      validateBinaryFile('revocation-order.pdf', 67658)
+
+      cy.task('readPdf', './cypress/downloads/revocation-order.pdf').then(({ numpages, text }) => {
+        expect(numpages, 'number of PDF pages').to.equal(1)
+        expect(text, 'has expected text').to.include('REVOCATION OF LICENCE')
+      })
+    })
   })
 
   function expectSearchResultsFromManageRecallsApi(expectedSearchTerm, expectedSearchResults) {
     cy.task('expectSearchResults', { expectedSearchTerm, expectedSearchResults })
+  }
+
+  function expectRevocationOrderFromManageRecallsApi(expectedPdfFile) {
+    cy.task('expectGenerateRevocationOrder', expectedPdfFile)
   }
 })
