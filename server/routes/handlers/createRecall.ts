93b38db825139da0323fa23aa896fdb2ef605fce
Jon Wyatt <unknown>
2021-07-06 12:59:16 +0100

    Add create recalls mock response and update recall ID key name

--- server/routes/handlers/createRecall.ts
+++ server/routes/handlers/createRecall.ts
@@ -6,8 +6,8 @@ export default function createRecallHandler(): RequestHandler {
   return async (req, res, next) => {
     const { nomsNumber } = req.body
     try {
-      const recallId = await createRecall(nomsNumber, res.locals.user.token)
-      res.redirect(303, `/offender-profile?nomsNumber=${nomsNumber}&recallId=${recallId.uuid}`)
+      const recall = await createRecall(nomsNumber, res.locals.user.token)
+      res.redirect(303, `/offender-profile?nomsNumber=${nomsNumber}&recallId=${recall.id}`)
     } catch (err) {
       logger.error(err)
       res.redirect(303, `/offender-profile?nomsNumber=${nomsNumber}`)
