e6efc482f00a18946752a74bae3f940d171273a4
NataliaOsk <natalia.oskina@equalexperts.com>
2021-07-06 16:36:50 +0100

    Pud-53: Fix

--- server/routes/handlers/generateRevocationOrder.ts
+++ server/routes/handlers/generateRevocationOrder.ts
@@ -1,16 +1,19 @@
-import type { RequestHandler } from 'express'
-import { generateRevocationOrder } from '../../clients/manageRecallsApi/manageRecallsApiClient'
+import type {RequestHandler} from 'express'
+import {generateRevocationOrder} from '../../clients/manageRecallsApi/manageRecallsApiClient'
 
 export default function generateRevocationOrderHandler(): RequestHandler {
-  return async (req, res, next) => {
-    const { nomsNumber } = req.query
-    const base64EncodedPdf = await generateRevocationOrder(nomsNumber as string, res.locals.user.token)
-    const pdfContentsByteArray = Buffer.from(base64EncodedPdf.content, 'base64')
+    return async (req, res, next) => {
+        const {nomsNumber} = req.query
+        if (typeof nomsNumber === 'undefined') {
+            return res.sendStatus(400)
+        }
+        const base64EncodedPdf = await generateRevocationOrder(nomsNumber as string, res.locals.user.token)
+        const pdfContentsByteArray = Buffer.from(base64EncodedPdf.content, 'base64')
 
-    res.writeHead(200, {
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': 'attachment; filename="revocation-order.pdf"',
-    })
-    res.end(pdfContentsByteArray)
-  }
+        res.writeHead(200, {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="revocation-order.pdf"',
+        })
+        res.end(pdfContentsByteArray)
+    }
 }
