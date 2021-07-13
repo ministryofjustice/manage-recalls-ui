b57d04f5b71fb8f25ff413155b1297609141a7f2
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-13 13:24:34 +0100

    PUD-101: Add pact json file.  Add new autogenerated types

--- server/@types/manage-recalls-api/index.d.ts
+++ server/@types/manage-recalls-api/index.d.ts
@@ -1,11 +1,13 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export type { BookRecallRequest } from './models/BookRecallRequest';
-export { ModelAndView } from './models/ModelAndView';
-export type { Mono_ResponseEntity_List_SearchResult_ } from './models/Mono_ResponseEntity_List_SearchResult_';
-export type { Pdf } from './models/Pdf';
-export type { RecallResponse } from './models/RecallResponse';
-export type { SearchRequest } from './models/SearchRequest';
-export type { SearchResult } from './models/SearchResult';
-export type { View } from './models/View';
+export type { BookRecallRequest } from './models/BookRecallRequest'
+export { ModelAndView } from './models/ModelAndView'
+export type { Mono_ResponseEntity_List_SearchResult_ } from './models/Mono_ResponseEntity_List_SearchResult_'
+export type { Mono_ResponseEntity_Pdf_ } from './models/Mono_ResponseEntity_Pdf_'
+export type { Pdf } from './models/Pdf'
+export type { RecallResponse } from './models/RecallResponse'
+export type { RevocationOrderRequest } from './models/RevocationOrderRequest'
+export type { SearchRequest } from './models/SearchRequest'
+export type { SearchResult } from './models/SearchResult'
+export type { View } from './models/View'
