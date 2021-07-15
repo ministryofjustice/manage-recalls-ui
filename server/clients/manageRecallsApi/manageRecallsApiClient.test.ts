3f5f6012bfa3179eff81c8af6477003d1506fdff
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-15 14:44:25 +0100

    PUD-101: Delete tests from manageRecallsApiClient.test.ts as they are covered in the pact tests

--- server/clients/manageRecallsApi/manageRecallsApiClient.test.ts
+++ server/clients/manageRecallsApi/manageRecallsApiClient.test.ts
@@ -1,11 +1,12 @@
 import nock from 'nock'
 
 import config from '../../config'
-import { searchByNomsNumber, generateRevocationOrder, createRecall } from './manageRecallsApiClient'
+import { generateRevocationOrder } from './manageRecallsApiClient'
 
 const token = { access_token: 'token-1', expires_in: 300 }
 const nomsNumber = 'NOMS_NUMBER'
 
+// TODO:  Delete this test once all endpoints have a pact test
 describe('manageRecallsApi', () => {
   let fakeManageRecallsApi: nock.Scope
 
@@ -16,28 +17,6 @@ describe('manageRecallsApi', () => {
   afterEach(() => {
     jest.resetAllMocks()
     nock.cleanAll()
-  })
-
-  describe('searchPrisoners', () => {
-    it('should return data from api', async () => {
-      const expectedResponse = [
-        {
-          firstName: 'Bertie',
-          lastName: 'Badger',
-          nomsNumber: '13AAA',
-          dateOfBirth: '1990-10-30',
-        },
-      ]
-
-      fakeManageRecallsApi
-        .post('/search')
-        .matchHeader('authorization', `Bearer ${token.access_token}`)
-        .reply(200, expectedResponse)
-
-      const actual = await searchByNomsNumber(nomsNumber, token.access_token)
-
-      expect(actual).toEqual(expectedResponse[0])
-    })
   })
 
   describe('generateRevocationOrder', () => {
@@ -51,21 +30,6 @@ describe('manageRecallsApi', () => {
         .reply(200, expectedResponse)
 
       const actual = await generateRevocationOrder(nomsNumber, token.access_token)
-
-      expect(actual).toEqual(expectedResponse)
-    })
-  })
-
-  describe('createRecallOrder', () => {
-    it('should create recall', async () => {
-      const expectedResponse = { contents: 1234 }
-
-      fakeManageRecallsApi
-        .post('/recalls')
-        .matchHeader('authorization', `Bearer ${token.access_token}`)
-        .reply(200, expectedResponse)
-
-      const actual = await createRecall('NOMS_NUMBER', token.access_token)
 
       expect(actual).toEqual(expectedResponse)
     })
