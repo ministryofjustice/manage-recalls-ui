8c00ac55fb1465c655e01f9ca219b9970d1ed99e
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-22 10:09:10 +0100

    PUD-276:  Update generated types

--- server/@types/manage-recalls-api/index.d.ts
+++ server/@types/manage-recalls-api/index.d.ts
@@ -7,7 +7,6 @@ export type { Mono_ResponseEntity_List_SearchResult_ } from './models/Mono_Respo
 export type { Mono_ResponseEntity_Pdf_ } from './models/Mono_ResponseEntity_Pdf_'
 export type { Pdf } from './models/Pdf'
 export type { RecallResponse } from './models/RecallResponse'
-export type { RevocationOrderRequest } from './models/RevocationOrderRequest'
 export type { SearchRequest } from './models/SearchRequest'
 export type { SearchResult } from './models/SearchResult'
 export type { View } from './models/View'
