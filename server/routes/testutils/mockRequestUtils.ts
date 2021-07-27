1d0cd6a8c80f34e73813ce6fd649f2d92a6ed026
Jon Wyatt <>
2021-07-27 09:52:51 +0100

    Restore lint comments

--- server/routes/testutils/mockRequestUtils.ts
+++ server/routes/testutils/mockRequestUtils.ts
@@ -2,6 +2,9 @@ import { getMockReq, getMockRes } from '@jest-mock/express'
 import { Request } from 'express'
 import { ObjectStrings, RequestQuery } from '../../@types/express'
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function mockPostRequest({ body, params }: { body?: ObjectStrings; params?: ObjectStrings }) {
   return getMockReq({
     body,
@@ -15,6 +18,7 @@ export function mockGetRequest(queryParams: RequestQuery): Request {
   })
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,camelcase
 export function mockResponseWithAuthenticatedUser(userAccessToken: string) {
   return getMockRes({
     locals: {
