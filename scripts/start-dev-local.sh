2b429fd35af22e154fc2c8d55a90358ddbf299b5
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-07-28 09:36:43 +0100

    PUD-260:  Update start-dev-local.sh to use npm run start:dev.  Add restart-fake-manage-recalls-api.sh to speed up restarting wiremock in dev mode

--- scripts/start-dev-local.sh
+++ scripts/start-dev-local.sh
@@ -8,16 +8,18 @@ readonly DOCKER_COMPOSE_FILE="$PROJECT_DIR/docker-compose.yml"
 readonly MANAGE_RECALLS_UI_NAME='manage-recalls-ui'
 readonly LOG_FILE="/tmp/${MANAGE_RECALLS_UI_NAME}.log"
 readonly FAKE_MANAGE_RECALLS_API_LOG_FILE="/tmp/fake-manage-recalls-api.log"
+readonly MANAGE_RECALLS_UI_PORT=3000
+readonly FAKE_MANAGE_RECALLS_API_PORT=9091
 
 docker compose -f "${DOCKER_COMPOSE_FILE}" up hmpps-auth redis -d --remove-orphans
 
-npx kill-port 3000 9091
+npx kill-port $FAKE_MANAGE_RECALLS_API_PORT $MANAGE_RECALLS_UI_PORT
 echo "Starting fake-manage-recalls-api, logs can be found here: ${FAKE_MANAGE_RECALLS_API_LOG_FILE}"
-java -jar fake-manage-recalls-api/wiremock-standalone-2.27.2.jar --port 9091 --verbose true --root-dir fake-manage-recalls-api/stubs >> "${FAKE_MANAGE_RECALLS_API_LOG_FILE}" 2>&1 &
+java -jar fake-manage-recalls-api/wiremock-standalone-2.27.2.jar --port ${FAKE_MANAGE_RECALLS_API_PORT} --verbose true --root-dir fake-manage-recalls-api/stubs >> "${FAKE_MANAGE_RECALLS_API_LOG_FILE}" 2>&1 &
 
 echo "Starting ${MANAGE_RECALLS_UI_NAME}, logs can be found here: ${LOG_FILE}"
 npm run build
-npm run start >> "${LOG_FILE}" 2>&1 &
+npm run start:dev >> "${LOG_FILE}" 2>&1 &
 
 printf "\nChecking hmpps-auth is running..."
 docker run --network container:hmpps-auth \
@@ -29,8 +31,8 @@ curl -s -4 --retry 10 -o /dev/null --retry-delay 1 --retry-connrefused http://lo
 echo "...done"
 echo
 echo "Logs can be found by running:"
-echo "  less /tmp/${LOG_FILE}"
-echo "  less /tmp/${FAKE_MANAGE_RECALLS_API_LOG_FILE}"
+echo "  less ${LOG_FILE}"
+echo "  less ${FAKE_MANAGE_RECALLS_API_LOG_FILE}"
 echo "  docker logs hmpps-auth"
 echo "  docker logs redis"
 echo "http://localhost:3000"
