b804515599667dee250f21bd06781ba0c3f86988
Jon Wyatt <>
2021-09-08 13:54:06 +0100

    Dossier check details page

--- server/@types/manage-recalls-api/models/AddDocumentRequest.ts
+++ server/@types/manage-recalls-api/models/AddDocumentRequest.ts
@@ -3,7 +3,18 @@
 /* eslint-disable */
 
 export type AddDocumentRequest = {
-  category: string
+  category: AddDocumentRequest.category
   fileContent: string
   fileName?: string
+}
+
+export namespace AddDocumentRequest {
+  export enum category {
+    LICENCE = 'LICENCE',
+    OASYS_RISK_ASSESSMENT = 'OASYS_RISK_ASSESSMENT',
+    PART_A_RECALL_REPORT = 'PART_A_RECALL_REPORT',
+    PREVIOUS_CONVICTIONS_SHEET = 'PREVIOUS_CONVICTIONS_SHEET',
+    PRE_SENTENCING_REPORT = 'PRE_SENTENCING_REPORT',
+    RECALL_NOTIFICATION_EMAIL = 'RECALL_NOTIFICATION_EMAIL',
+  }
 }
