96a8580d2d4f80c5df89b875decd6da7003446d8
Jon Wyatt <>
2021-09-09 15:52:46 +0100

    Update types with dossier email

--- server/@types/manage-recalls-api/models/ApiRecallDocument.ts
+++ server/@types/manage-recalls-api/models/ApiRecallDocument.ts
@@ -10,6 +10,7 @@ export type ApiRecallDocument = {
 
 export namespace ApiRecallDocument {
   export enum category {
+    DOSSIER_EMAIL = 'DOSSIER_EMAIL',
     LICENCE = 'LICENCE',
     OASYS_RISK_ASSESSMENT = 'OASYS_RISK_ASSESSMENT',
     PART_A_RECALL_REPORT = 'PART_A_RECALL_REPORT',
