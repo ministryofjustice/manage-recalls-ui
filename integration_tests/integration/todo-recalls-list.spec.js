ada671dbc8fee09ab7822d101d47dcb5f10b4456
Jon Wyatt <>
2021-09-14 10:49:34 +0100

    Update 'Continue booking' link from recall list + int test

---
+++ integration_tests/integration/todo-recalls-list.spec.js
@@ -0,0 +1,75 @@
+import { searchResponse } from '../mockApis/mockResponses'
+import recallsListPage from '../pages/recallsList'
+import assessRecallPage from '../pages/assessRecall'
+import dossierLetterPage from '../pages/dossierLetter'
+
+const recallPreConsNamePage = require('../pages/recallPreConsName')
+
+context('To do recalls list', () => {
+  const recallId = '123'
+  const personName = `${searchResponse[0].firstName} ${searchResponse[0].lastName}`
+  let recallPreConsName
+
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubLogin')
+    cy.task('stubAuthUser')
+    cy.task('expectSearchResults', { expectedSearchTerm: nomsNumber, expectedSearchResults: searchResponse })
+    cy.task('expectCreateRecall', { expectedResults: { recallId } })
+    cy.task('expectUpdateRecall', recallId)
+    cy.task('expectAddRecallDocument', { statusCode: 201 })
+    cy.task('expectGetRecall', { recallId, expectedResult: { recallId, documents: [] } })
+  })
+
+  const nomsNumber = 'A1234AA'
+
+  it('User can continue a previous booking', () => {
+    cy.task('expectListRecalls', {
+      expectedResults: [
+        {
+          recallId,
+          nomsNumber,
+        },
+      ],
+    })
+    cy.login()
+    const recallsList = recallsListPage.verifyOnPage()
+    recallsList.expectActionLinkText({ id: `continue-booking-${recallId}`, text: 'Continue booking' })
+    recallsList.continueBooking({ recallId })
+    recallPreConsName = recallPreConsNamePage.verifyOnPage({ personName })
+  })
+
+  it('User can assess a recall', () => {
+    cy.task('expectListRecalls', {
+      expectedResults: [
+        {
+          recallId,
+          nomsNumber,
+          status: 'BOOKED_ON',
+        },
+      ],
+    })
+    cy.login()
+    const recallsList = recallsListPage.verifyOnPage()
+    recallsList.expectActionLinkText({ id: `assess-recall-${recallId}`, text: 'Assess recall' })
+    recallsList.assessRecall({ recallId })
+    assessRecallPage.verifyOnPage({ fullName: personName })
+  })
+
+  it('User can create a dossier', () => {
+    cy.task('expectListRecalls', {
+      expectedResults: [
+        {
+          recallId,
+          nomsNumber,
+          status: 'RECALL_NOTIFICATION_ISSUED',
+        },
+      ],
+    })
+    cy.login()
+    const recallsList = recallsListPage.verifyOnPage()
+    recallsList.expectActionLinkText({ id: `create-dossier-${recallId}`, text: 'Create dossier' })
+    recallsList.createDossier({ recallId })
+    dossierLetterPage.verifyOnPage()
+  })
+})
