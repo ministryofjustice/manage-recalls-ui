fea4db00d4ac53528a6db5c8aff7583f8d641703
Jon Wyatt <>
2021-09-20 11:37:36 +0100

    Error handling / message

--- server/routes/handlers/user/userDetails.ts
+++ server/routes/handlers/user/userDetails.ts
@@ -1,11 +1,33 @@
 import { Request, Response } from 'express'
 import { getUserDetails, addUserDetails } from '../../../clients/manageRecallsApi/manageRecallsApiClient'
+import logger from '../../../../logger'
 
 export const getUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { uuid, token } = res.locals.user
     const user = await getUserDetails(uuid, token)
     res.locals.user = { ...res.locals.user, ...user }
+  } catch (err) {
+    if (err.status !== 404) {
+      logger.info(err.message)
+      res.locals.errors = {
+        list: [
+          {
+            name: 'error',
+            text: 'An error occurred fetching your details',
+          },
+        ],
+      }
+    } else {
+      res.locals.errors = {
+        list: [
+          {
+            name: 'error',
+            text: 'User details not found',
+          },
+        ],
+      }
+    }
   } finally {
     res.render(`pages/userDetails`)
   }
