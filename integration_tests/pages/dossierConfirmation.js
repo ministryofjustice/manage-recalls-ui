e2936c9546772dd852c0a43dee25838d5f497964
Jon Wyatt <jonnywyatt2@gmail.com>
2021-09-21 11:47:47 +0100

    Content updates (#204)
    Co-authored-by: Jon Wyatt <>

--- integration_tests/pages/dossierConfirmation.js
+++ integration_tests/pages/dossierConfirmation.js
@@ -1,7 +1,7 @@
 const page = require('./page')
 
 const createDossierConfirmationPage = ({ nomsNumber, recallId } = {}) =>
-  page('Dossier creation completed', {
+  page('Dossier created and sent', {
     url: recallId ? `/persons/${nomsNumber}/recalls/${recallId}/dossier-confirmation` : null,
   })
 
