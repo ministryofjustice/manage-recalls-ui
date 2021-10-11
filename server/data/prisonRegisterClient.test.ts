150afc4cbc16ad52823dc4746004ab4ffa6d3de8
Mike Dunford <mike.dunford@digital.justice.gov.uk>
2021-10-11 14:22:08 +0100

    PUD-787:  Add findPrisonById to prisonRegisterClient.

--- server/data/prisonRegisterClient.test.ts
+++ server/data/prisonRegisterClient.test.ts
@@ -1,87 +1,95 @@
-import { getPrisonList } from './prisonRegisterClient'
-import RestClient from './restClient'
-import { Prison } from '../@types'
+import nock from 'nock'
+import { findPrisonById, getPrisonList } from './prisonRegisterClient'
+import config from '../config'
 
-jest.mock('./restClient')
+describe('prisonRegisterClient', () => {
+  let fakePrisonRegisterApi: nock.Scope
 
-describe('getActivePrisonList', () => {
-  it('returns a list of all prisons if successful', async () => {
-    ;(RestClient as jest.Mock).mockImplementation(() => {
-      return {
-        get: async (): Promise<Prison[]> => [
-          {
-            prisonId: 'ALI',
-            prisonName: 'Albany (HMP)',
-            active: false,
-          },
-          {
-            prisonId: 'AKI',
-            prisonName: 'Acklington (HMP)',
-            active: true,
-          },
-        ],
-      }
+  beforeEach(() => {
+    fakePrisonRegisterApi = nock(config.apis.prisonRegister.url)
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  describe('getActivePrisonList', () => {
+    it('returns a list of all prisons if successful', async () => {
+      const expectedPrisons = [
+        {
+          prisonId: 'AKI',
+          prisonName: 'Acklington (HMP)',
+          active: true,
+        },
+        {
+          prisonId: 'ALI',
+          prisonName: 'Albany (HMP)',
+          active: false,
+        },
+      ]
+      fakePrisonRegisterApi.get(`/prisons`).reply(200, expectedPrisons)
+
+      const prisons = await getPrisonList()
+      expect(prisons).toEqual(expectedPrisons)
     })
-    const prisons = await getPrisonList()
-    expect(prisons).toEqual([
-      {
+
+    it('alphabetically sorts the list by prison name', async () => {
+      const belmarshPrison = {
+        prisonId: 'BEL',
+        prisonName: 'Belmarsh (HMP)',
+        active: true,
+      }
+      const kennetPrison = {
+        prisonId: 'KEN',
+        prisonName: 'Kennet (HMP)',
+        active: true,
+      }
+      const acklingtonPrison = {
         prisonId: 'AKI',
         prisonName: 'Acklington (HMP)',
         active: true,
-      },
-      {
-        prisonId: 'ALI',
+      }
+      fakePrisonRegisterApi.get(`/prisons`).reply(200, [belmarshPrison, kennetPrison, acklingtonPrison])
+      const prisons = await getPrisonList()
+      expect(prisons).toEqual([acklingtonPrison, belmarshPrison, kennetPrison])
+    })
+
+    it('returns undefined if no prisons are returned', async () => {
+      fakePrisonRegisterApi.get(`/prisons`).reply(200, [])
+      const prisons = await getPrisonList()
+      expect(prisons).toBeUndefined()
+    })
+
+    it('returns undefined if the request fails', async () => {
+      fakePrisonRegisterApi.get(`/prisons`).replyWithError('Boom')
+      const prisons = await getPrisonList()
+      expect(prisons).toBeUndefined()
+    })
+  })
+
+  describe('findPrisonById', () => {
+    it('can retrieve a prison by id', async () => {
+      const prisonId = 'ALI'
+      const expectedPrison = {
+        prisonId,
         prisonName: 'Albany (HMP)',
         active: false,
-      },
-    ])
-  })
-
-  it('alphabetically sorts the list by prison name', async () => {
-    ;(RestClient as jest.Mock).mockImplementation(() => {
-      return {
-        get: async (): Promise<Prison[]> => [
-          {
-            prisonId: 'BEL',
-            prisonName: 'Belmarsh (HMP)',
-            active: true,
-          },
-          {
-            prisonId: 'KEN',
-            prisonName: 'Kennet (HMP)',
-            active: true,
-          },
-          {
-            prisonId: 'AKI',
-            prisonName: 'Acklington (HMP)',
-            active: true,
-          },
-        ],
       }
+      fakePrisonRegisterApi.get(`/prisons/id/${prisonId}`).reply(200, expectedPrison)
+      const prison = await findPrisonById(prisonId)
+      expect(prison).toEqual(expectedPrison)
     })
-    const prisons = await getPrisonList()
-    expect(prisons.map(p => p.prisonName)).toEqual(['Acklington (HMP)', 'Belmarsh (HMP)', 'Kennet (HMP)'])
-  })
 
-  it('returns undefined if no prisons are returned', async () => {
-    ;(RestClient as jest.Mock).mockImplementation(() => {
-      return {
-        get: async (): Promise<Prison[]> => [],
-      }
+    it('returns undefined if no prison is found', async () => {
+      fakePrisonRegisterApi.get(`/prisons/id/XXX`).reply(404)
+      const prison = await findPrisonById('XXX')
+      expect(prison).toBeUndefined()
     })
-    const prisons = await getPrisonList()
-    expect(prisons).toBeUndefined()
-  })
 
-  it('returns undefined if the request fails', async () => {
-    ;(RestClient as jest.Mock).mockImplementation(() => {
-      return {
-        get: async (): Promise<Prison[]> => {
-          throw new Error('Timeout')
-        },
-      }
+    it('returns undefined if the request fails', async () => {
+      fakePrisonRegisterApi.get('/prisons/id/XXX').replyWithError('Boom')
+      const prison = await findPrisonById('XXX')
+      expect(prison).toBeUndefined()
     })
-    const prisons = await getPrisonList()
-    expect(prisons).toBeUndefined()
   })
 })
--- server/data/prisonRegisterClient.ts
+++ server/data/prisonRegisterClient.ts
@@ -6,8 +6,7 @@ import { Prison } from '../@types'
 export const getPrisonList = async (): Promise<Prison[] | undefined> => {
   logger.info(`Getting prison list`)
   try {
-    const client = new RestClient('Prison register client', config.apis.prisonRegister)
-    const prisons = await client.get<Prison[]>({ path: '/prisons' })
+    const prisons = await restClient().get<Prison[]>({ path: '/prisons' })
     if (prisons?.length) {
       return prisons.sort((a, b) => (a.prisonName < b.prisonName ? -1 : 1))
     }
@@ -17,3 +16,15 @@ export const getPrisonList = async (): Promise<Prison[] | undefined> => {
     return undefined
   }
 }
+
+export async function findPrisonById(prisonId: string): Promise<Prison | undefined> {
+  logger.info(`Finding prison for ${prisonId}`)
+  try {
+    return await restClient().get<Prison>({ path: `/prisons/id/${prisonId}` })
+  } catch (err) {
+    logger.error(err)
+    return undefined
+  }
+}
+
+const restClient = () => new RestClient('Prison register client', config.apis.prisonRegister)
