b0eb7f76c9d6db0fd8644c5ae2997004733eff5b
Phil Segal <philsegal@users.noreply.github.com>
2021-10-21 15:38:01 +0100

    PUD-738: Fix PACT for recall assignee (#312)

--- pact/deleteAssessmentAssignee.pact.test.ts
+++ pact/deleteAssessmentAssignee.pact.test.ts
@@ -8,8 +8,8 @@ import { pactJsonResponse, pactDeleteRequest } from './pactTestUtils'
 
 pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, provider => {
   const accessToken = 'accessToken-1'
-  const userId = '00000000-0000-0000-0000-000000000000'
-  const recallId = '123-456'
+  const userId = '11111111-1111-1111-1111-111111111111'
+  const recallId = '00000000-0000-0000-0000-000000000000'
 
   beforeEach(() => {
     jest.spyOn(configModule, 'manageRecallsApiConfig').mockReturnValue({ url: provider.mockService.baseUrl })
@@ -17,7 +17,7 @@ pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, prov
 
   describe('unassign user from recall assessment', () => {
     test('can assign a user', async () => {
-      const recallNoAssignee = { ...getRecallResponseJson, assignee: undefined }
+      const recallNoAssignee = { ...getRecallResponseJson, assignee: undefined, assigneeUserName: undefined }
       await provider.addInteraction({
         state: 'a user can be unassigned',
         ...pactDeleteRequest('an unassign user request', `/recalls/${recallId}/assignee/${userId}`, accessToken),
@@ -27,28 +27,6 @@ pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, prov
       const actual = await deleteAssessingUser(recallId, userId, accessToken)
 
       expect(actual).toEqual(recallNoAssignee)
-    })
-
-    test('returns 400 if blank userId provided', async () => {
-      const blankUserId = ''
-      const errorResponse = {
-        status: 'BAD_REQUEST',
-      }
-      await provider.addInteraction({
-        state: 'a missing user ID',
-        ...pactDeleteRequest(
-          'an unassign user request with blank userId',
-          `/recalls/${recallId}/assignee/${blankUserId}`,
-          accessToken
-        ),
-        willRespondWith: pactJsonResponse(Matchers.like(errorResponse), 400),
-      })
-
-      try {
-        await deleteAssessingUser(recallId, blankUserId, accessToken)
-      } catch (exception) {
-        expect(exception.status).toEqual(400)
-      }
     })
 
     test('returns 401 if invalid user', async () => {
