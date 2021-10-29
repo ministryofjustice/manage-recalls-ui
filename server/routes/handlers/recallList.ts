da5739f6fe9e486a641394e9c2f82283d3ac5837
Jon Wyatt <jonnywyatt2@gmail.com>
2021-10-29 12:26:04 +0100

    Send timings for recall list to appInsights (#333)
    * Send timings for recall list to prometheus
    
    * Use summary not gauge
    
    * Change to appInsights
    
    * Use created client / don't error if undefined
    
    * Import perf_hooks
    
    Co-authored-by: Jon Wyatt <>

--- server/routes/handlers/recallList.ts
+++ server/routes/handlers/recallList.ts
@@ -1,14 +1,20 @@
 import { Request, Response } from 'express'
+import { performance } from 'perf_hooks'
+import { buildAppInsightsClient } from '../../utils/azureAppInsights'
 import { getRecallList, searchByNomsNumber } from '../../clients/manageRecallsApi/manageRecallsApiClient'
 import { RecallResult } from '../../@types'
 
 export const recallList = async (req: Request, res: Response): Promise<Response | void> => {
+  const appInsightsClient = buildAppInsightsClient()
   const { token } = res.locals.user
+  const start = performance.now()
   const recalls = await getRecallList(token)
+  appInsightsClient?.trackMetric({ name: 'getRecallList', value: Math.round(performance.now() - start) })
   const recallsWithNomsNumbers = recalls.filter(recall => Boolean(recall.nomsNumber))
   const successful = [] as RecallResult[]
   const failed = [] as string[]
   if (recallsWithNomsNumbers.length) {
+    const start2 = performance.now()
     const results = await Promise.allSettled(
       recallsWithNomsNumbers.map(recall =>
         searchByNomsNumber(recall.nomsNumber, token).then(
@@ -20,6 +26,8 @@ export const recallList = async (req: Request, res: Response): Promise<Response 
         )
       )
     )
+    appInsightsClient?.trackMetric({ name: 'searchByNomsNumber_total', value: Math.round(performance.now() - start2) })
+    appInsightsClient?.trackMetric({ name: 'searchByNomsNumber_call_count', value: recallsWithNomsNumbers.length })
     results.forEach((result, idx) => {
       if (result.status === 'fulfilled') {
         successful.push(result.value)
