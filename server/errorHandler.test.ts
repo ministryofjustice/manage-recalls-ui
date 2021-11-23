781a64ca8b2e51da5fd0c1cad21d47ad3ea54b82
Jon Wyatt <>
2021-11-23 08:34:05 +0000

    Error pages

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -1,6 +1,9 @@
 import type { Express } from 'express'
 import request from 'supertest'
 import appWithAllRoutes from './routes/testutils/appSetup'
+import { recallList } from './routes/handlers/recallList'
+
+jest.mock('./routes/handlers/recallList')
 
 let app: Express
 
@@ -32,6 +35,32 @@ describe('GET 404', () => {
       .expect(res => {
         expect(res.text).toContain('Something went wrong. The error has been logged. Please try again')
         expect(res.text).not.toContain('NotFoundError: Not found')
+      })
+  })
+})
+
+describe('Server error', () => {
+  beforeEach(() => (recallList as jest.Mock).mockRejectedValue(new Error('Something went wrong')))
+
+  it('should render content with stack in dev mode', () => {
+    return request(app)
+      .get('/')
+      .expect(500)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Sorry, there is a problem with the service')
+        expect(res.text).toContain('Something went wrong')
+      })
+  })
+
+  it('should render content without stack in production mode', () => {
+    return request(appWithAllRoutes({ production: true }))
+      .get('/')
+      .expect(500)
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Sorry, there is a problem with the service')
+        expect(res.text).not.toContain('Something went wrong')
       })
   })
 })
