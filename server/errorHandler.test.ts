6b3f2e358733b727859e4f4322dec86dd582da73
Jon Wyatt <>
2021-11-23 10:04:24 +0000

    Update 404 test

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -22,19 +22,7 @@ describe('GET 404', () => {
       .expect(404)
       .expect('Content-Type', /html/)
       .expect(res => {
-        expect(res.text).toContain('NotFoundError: Not found')
-        expect(res.text).not.toContain('Something went wrong. The error has been logged. Please try again')
-      })
-  })
-
-  it('should render content without stack in production mode', () => {
-    return request(appWithAllRoutes({ production: true }))
-      .get('/unknown')
-      .expect(404)
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('Something went wrong. The error has been logged. Please try again')
-        expect(res.text).not.toContain('NotFoundError: Not found')
+        expect(res.text).toContain('Page not found')
       })
   })
 })
