9b0de3cb79ec7f49a3a184632cabd1697251d7c3
Jon Wyatt <>
2021-11-26 10:13:32 +0000

    Send names in create recall request

--- server/routes/handlers/book/createRecall.test.ts
+++ server/routes/handlers/book/createRecall.test.ts
@@ -1,29 +1,24 @@
-import nock from 'nock'
 import { mockPostRequest, mockResponseWithAuthenticatedUser } from '../../testutils/mockRequestUtils'
-import config from '../../../config'
 import { createRecall } from './createRecall'
+import { getPerson } from '../helpers/personCache'
+import { createRecall as createRecallApi } from '../../../clients/manageRecallsApi/manageRecallsApiClient'
+
+jest.mock('../../../clients/manageRecallsApi/manageRecallsApiClient')
+jest.mock('../helpers/personCache')
 
 const userToken = { access_token: 'token-1', expires_in: 300 }
 
 describe('createRecall', () => {
-  let fakeManageRecallsApi: nock.Scope
+  const person = {
+    firstName: 'Barry',
+    lastName: 'Badger',
+  }
+  const nomsNumber = '123ABC'
 
-  beforeEach(() => {
-    fakeManageRecallsApi = nock(config.apis.manageRecallsApi.url)
-  })
-
-  afterEach(() => {
-    nock.cleanAll()
-  })
-
-  it('should return a recall ID', async () => {
+  it("redirects to pre cons if the person doesn't have a middle name", async () => {
     const recallId = '123'
-    const nomsNumber = '123ABC'
-
-    fakeManageRecallsApi
-      .post('/recalls')
-      .matchHeader('authorization', `Bearer ${userToken.access_token}`)
-      .reply(200, { recallId })
+    ;(getPerson as jest.Mock).mockResolvedValue(person)
+    ;(createRecallApi as jest.Mock).mockResolvedValue({ recallId })
 
     const req = mockPostRequest({ params: { nomsNumber } })
     const { res } = mockResponseWithAuthenticatedUser(userToken.access_token)
@@ -31,5 +26,33 @@ describe('createRecall', () => {
     await createRecall(req, res)
 
     expect(res.redirect).toHaveBeenCalledWith(303, `/persons/${nomsNumber}/recalls/${recallId}/pre-cons-name`)
+  })
+
+  it('redirects to licence name if the person does have a middle name', async () => {
+    const recallId = '123'
+    ;(getPerson as jest.Mock).mockResolvedValue({ ...person, middleNames: 'Bryan' })
+    ;(createRecallApi as jest.Mock).mockResolvedValue({ recallId })
+
+    const req = mockPostRequest({ params: { nomsNumber } })
+    const { res } = mockResponseWithAuthenticatedUser(userToken.access_token)
+
+    await createRecall(req, res)
+
+    expect(res.redirect).toHaveBeenCalledWith(303, `/persons/${nomsNumber}/recalls/${recallId}/licence-name`)
+  })
+
+  it("doesn't catch an error thrown if createRecall fails", async () => {
+    ;(getPerson as jest.Mock).mockResolvedValue({ ...person, middleNames: 'Bryan' })
+    ;(createRecallApi as jest.Mock).mockRejectedValue(new Error('Timeout'))
+
+    const req = mockPostRequest({ params: { nomsNumber } })
+    const { res } = mockResponseWithAuthenticatedUser(userToken.access_token)
+
+    try {
+      await createRecall(req, res)
+    } catch (err) {
+      expect(res.redirect).not.toHaveBeenCalled()
+      expect(err.message).toEqual('Timeout')
+    }
   })
 })
