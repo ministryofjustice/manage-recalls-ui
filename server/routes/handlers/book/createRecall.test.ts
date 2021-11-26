d8a9a4082cb9efd9d9eaef703cc9cea8c0e30f01
Jon Wyatt <>
2021-11-26 10:32:51 +0000

    Don't redirect to licence

--- server/routes/handlers/book/createRecall.test.ts
+++ server/routes/handlers/book/createRecall.test.ts
@@ -15,7 +15,7 @@ describe('createRecall', () => {
   }
   const nomsNumber = '123ABC'
 
-  it("redirects to pre cons if the person doesn't have a middle name", async () => {
+  it('redirects to pre cons', async () => {
     const recallId = '123'
     ;(getPerson as jest.Mock).mockResolvedValue(person)
     ;(createRecallApi as jest.Mock).mockResolvedValue({ recallId })
@@ -27,19 +27,19 @@ describe('createRecall', () => {
 
     expect(res.redirect).toHaveBeenCalledWith(303, `/persons/${nomsNumber}/recalls/${recallId}/pre-cons-name`)
   })
-
-  it('redirects to licence name if the person does have a middle name', async () => {
-    const recallId = '123'
-    ;(getPerson as jest.Mock).mockResolvedValue({ ...person, middleNames: 'Bryan' })
-    ;(createRecallApi as jest.Mock).mockResolvedValue({ recallId })
-
-    const req = mockPostRequest({ params: { nomsNumber } })
-    const { res } = mockResponseWithAuthenticatedUser(userToken.access_token)
-
-    await createRecall(req, res)
-
-    expect(res.redirect).toHaveBeenCalledWith(303, `/persons/${nomsNumber}/recalls/${recallId}/licence-name`)
-  })
+  //
+  // it('redirects to licence name if the person does have a middle name', async () => {
+  //   const recallId = '123'
+  //   ;(getPerson as jest.Mock).mockResolvedValue({ ...person, middleNames: 'Bryan' })
+  //   ;(createRecallApi as jest.Mock).mockResolvedValue({ recallId })
+  //
+  //   const req = mockPostRequest({ params: { nomsNumber } })
+  //   const { res } = mockResponseWithAuthenticatedUser(userToken.access_token)
+  //
+  //   await createRecall(req, res)
+  //
+  //   expect(res.redirect).toHaveBeenCalledWith(303, `/persons/${nomsNumber}/recalls/${recallId}/licence-name`)
+  // })
 
   it("doesn't catch an error thrown if createRecall fails", async () => {
     ;(getPerson as jest.Mock).mockResolvedValue({ ...person, middleNames: 'Bryan' })
