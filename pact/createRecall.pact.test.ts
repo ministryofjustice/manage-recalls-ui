a6824afbc4b739f3a28a6daad0120b8c5d502bd3
Jon Wyatt <>
2021-11-26 11:25:48 +0000

    PACT

--- pact/createRecall.pact.test.ts
+++ pact/createRecall.pact.test.ts
@@ -8,7 +8,12 @@ import { pactJsonResponse, pactPostRequest } from './pactTestUtils'
 
 pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, provider => {
   const accessToken = 'accessToken-1'
-  const nomsNumber = 'A1234AA'
+  const requestBody = {
+    nomsNumber: 'A1234AA',
+    firstName: 'Bobby',
+    lastName: 'Badger',
+    middleNames: 'Bryan',
+  }
 
   beforeEach(() => {
     jest.spyOn(configModule, 'manageRecallsApiConfig').mockReturnValue({ url: provider.mockService.baseUrl })
@@ -18,34 +23,34 @@ pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, prov
     test('can successfully create a recall', async () => {
       await provider.addInteraction({
         state: 'no state required',
-        ...pactPostRequest('a create recall request', '/recalls', { nomsNumber }, accessToken),
+        ...pactPostRequest('a create recall request', '/recalls', requestBody, accessToken),
         willRespondWith: pactJsonResponse(Matchers.like(createRecallResponseJson), 201),
       })
 
-      const actual = await createRecall(nomsNumber, accessToken)
+      const actual = await createRecall(requestBody, accessToken)
 
       expect(actual).toEqual(createRecallResponseJson)
     })
 
-    test('returns 400 if blank NOMS number provided', async () => {
-      const blankNomsNumber = ''
+    test('returns 400 if blank fields provided', async () => {
+      const blankBody = {
+        nomsNumber: '',
+        firstName: '',
+        lastName: '',
+        middleNames: '',
+      }
       const errorResponse = {
         status: 'BAD_REQUEST',
         message: 'nomsNumber: must not be blank',
       }
       await provider.addInteraction({
         state: 'no state required',
-        ...pactPostRequest(
-          'a create recall request with blank nomsNumber',
-          '/recalls',
-          { nomsNumber: blankNomsNumber },
-          accessToken
-        ),
+        ...pactPostRequest('a create recall request with blank nomsNumber', '/recalls', blankBody, accessToken),
         willRespondWith: pactJsonResponse(Matchers.like(errorResponse), 400),
       })
 
       try {
-        await createRecall(blankNomsNumber, accessToken)
+        await createRecall(blankBody, accessToken)
       } catch (exception) {
         expect(exception.status).toEqual(400)
         expect(exception.data).toEqual(errorResponse)
@@ -55,12 +60,12 @@ pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, prov
     test('returns 401 if invalid user', async () => {
       await provider.addInteraction({
         state: 'an unauthorized user accessToken',
-        ...pactPostRequest('an unauthorized create recall request', '/recalls', { nomsNumber }, accessToken),
+        ...pactPostRequest('an unauthorized create recall request', '/recalls', requestBody, accessToken),
         willRespondWith: { status: 401 },
       })
 
       try {
-        await createRecall(nomsNumber, accessToken)
+        await createRecall(requestBody, accessToken)
       } catch (exception) {
         expect(exception.status).toEqual(401)
       }
--- server/routes/handlers/book/createRecall.ts
+++ server/routes/handlers/book/createRecall.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { createRecall as createRecallApi } from '../../../clients/manageRecallsApi/manageRecallsApiClient'
-import logger from '../../../../logger'
+import { getPerson } from '../helpers/personCache'
 
 export const createRecall = async (req: Request, res: Response): Promise<void> => {
   const { nomsNumber } = req.params
@@ -9,14 +9,8 @@ export const createRecall = async (req: Request, res: Response): Promise<void> =
     return
   }
   const { user } = res.locals
-  try {
-    const recall = await createRecallApi(nomsNumber, user.token)
-    if (!recall.recallId) {
-      throw new Error("Created recall didn't return a recallId")
-    }
-    res.redirect(303, `/persons/${nomsNumber}/recalls/${recall.recallId}/pre-cons-name`)
-  } catch (err) {
-    logger.error(err)
-    res.redirect(303, `/persons/${nomsNumber}`)
-  }
+  const { firstName, lastName, middleNames } = await getPerson(nomsNumber, user.token)
+  const { recallId } = await createRecallApi({ firstName, lastName, middleNames, nomsNumber }, user.token)
+  const nextPageSuffix = 'pre-cons-name' // middleNames ? 'licence-name' : 'pre-cons-name'
+  res.redirect(303, `/persons/${nomsNumber}/recalls/${recallId}/${nextPageSuffix}`)
 }
