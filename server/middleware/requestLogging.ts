a698d1737c5e143d5181f701173b139ddf505077
Jon Wyatt <>
2021-11-30 16:35:27 +0000

    Request logging

---
+++ server/middleware/requestLogging.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express'
+import logger from '../../logger'
+
+export const requestLogging = (req: Request, res: Response, next: NextFunction) => {
+  const userId = res.locals.user?.uuid
+  const { appInsightsOperationId } = res.locals
+  const sessionId = req.session.id
+  const pageUrl = req.originalUrl
+  logger.debug(
+    {
+      requestData: {
+        userId,
+        appInsightsOperationId,
+        sessionId,
+        pageUrl,
+      },
+    },
+    'request data'
+  )
+  next()
+}
--- server/routes/standardRouter.ts
+++ server/routes/standardRouter.ts
@@ -4,12 +4,14 @@ import auth from '../authentication/auth'
 import tokenVerifier from '../data/tokenVerification'
 import populateCurrentUser from '../middleware/populateCurrentUser'
 import type UserService from '../services/userService'
+import { requestLogging } from '../middleware/requestLogging'
 
 export default function standardRouter(userService: UserService): Router {
   const router = Router({ mergeParams: true })
 
   router.use(auth.authenticationMiddleware(tokenVerifier))
   router.use(populateCurrentUser(userService))
+  router.use(requestLogging)
   router.use((req, res, next) => {
     if (typeof req.csrfToken === 'function') {
       res.locals.csrfToken = req.csrfToken()
