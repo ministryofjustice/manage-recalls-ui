abb7435725f3e9243257267df392b045fffba9d5
Jon Wyatt <jonnywyatt2@gmail.com>
2021-12-07 08:48:39 +0000

    Redirect to user details if none exist (#450)
    Co-authored-by: Jon Wyatt <>

---
+++ server/middleware/checkUserDetailsExist.test.ts
@@ -0,0 +1,55 @@
+// @ts-nocheck
+import { checkUserDetailsExist } from './checkUserDetailsExist'
+import { getCurrentUserDetails } from '../clients/manageRecallsApi/manageRecallsApiClient'
+
+jest.mock('../clients/manageRecallsApi/manageRecallsApiClient')
+
+describe('checkUserDetailsExist', () => {
+  let req
+  const token = '123'
+  const res = {
+    locals: {},
+  }
+  let next
+  const userDetails = {
+    firstName: 'Barry',
+    lastName: 'Badger',
+    email: 'barry@badger.com',
+    phoneNumber: '0739378378',
+    caseworkerBand: 'FOUR_PLUS',
+    signature: 'def',
+    userId: '123',
+  }
+
+  beforeEach(() => {
+    req = {
+      url: '/',
+    }
+    res.locals.user = {
+      token,
+    }
+    res.redirect = jest.fn()
+    next = jest.fn()
+  })
+
+  afterEach(() => jest.resetAllMocks())
+
+  it('calls manage recalls APIs, then calls next middleware, if user present on res.locals', async () => {
+    ;(getCurrentUserDetails as jest.Mock).mockResolvedValue(userDetails)
+    await checkUserDetailsExist(req, res, next)
+    expect(getCurrentUserDetails).toHaveBeenCalledWith(token)
+    expect(next).toHaveBeenCalled()
+  })
+
+  it('redirects to user details page if the user details are not present', async () => {
+    ;(getCurrentUserDetails as jest.Mock).mockRejectedValue({ data: { status: 'NOT_FOUND' } })
+    await checkUserDetailsExist(req, res, next)
+    expect(res.redirect).toHaveBeenCalledWith('/user-details')
+  })
+
+  it('calls next if the user details call errors with a code other than 404', async () => {
+    ;(getCurrentUserDetails as jest.Mock).mockRejectedValue({ data: { status: 'SERVER_ERROR' } })
+    await checkUserDetailsExist(req, res, next)
+    expect(next).toHaveBeenCalled()
+  })
+})
---
+++ server/middleware/checkUserDetailsExist.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express'
+import { getCurrentUserDetails } from '../clients/manageRecallsApi/manageRecallsApiClient'
+import logger from '../../logger'
+
+export const checkUserDetailsExist = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { token } = res.locals.user
+    await getCurrentUserDetails(token)
+    next()
+  } catch (err) {
+    if (err.data?.status === 'NOT_FOUND') {
+      logger.info(err.data?.reason, `Error retrieving user details from manage-recalls-api`)
+      return res.redirect('/user-details')
+    }
+    next()
+  }
+}
