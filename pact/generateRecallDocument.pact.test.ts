fbc5ee991ce7feec8e35100c0e06b9c110409186
Jon Wyatt <>
2021-12-15 14:04:52 +0000

    Revocation order

--- pact/generateRecallDocument.pact.test.ts
+++ pact/generateRecallDocument.pact.test.ts
@@ -9,7 +9,7 @@ import { pactPostRequest, pactJsonResponse } from './pactTestUtils'
 pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, provider => {
   const accessToken = 'accessToken-1'
   const recallId = '00000000-0000-0000-0000-000000000000'
-  const category = 'RECALL_NOTIFICATION'
+  const category = 'REVOCATION_ORDER'
   const detail = 'Details changed.'
   const url = `/recalls/${recallId}/documents/generated`
   const requestBody = { category, detail }
