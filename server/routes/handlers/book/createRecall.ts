98cfb714be92f4c70ba6015b241f11e81fa088ce
Phil Segal <phil@phisec.co.uk>
2022-01-28 09:27:49 +0000

    PUD-1399: Send CRO Number & DateOfBirth in BookRecallRequest

--- server/routes/handlers/book/createRecall.ts
+++ server/routes/handlers/book/createRecall.ts
@@ -9,8 +9,11 @@ export const createRecall = async (req: Request, res: Response): Promise<void> =
     return
   }
   const { user } = res.locals
-  const { firstName, lastName, middleNames } = await getPerson(nomsNumber, user.token)
-  const { recallId } = await createRecallApi({ firstName, lastName, middleNames, nomsNumber }, user.token)
+  const { firstName, lastName, middleNames, croNumber, dateOfBirth } = await getPerson(nomsNumber, user.token)
+  const { recallId } = await createRecallApi(
+    { firstName, lastName, middleNames, nomsNumber, croNumber, dateOfBirth },
+    user.token
+  )
   const nextPageSuffix = middleNames ? 'licence-name' : 'pre-cons-name'
   res.redirect(303, `/persons/${nomsNumber}/recalls/${recallId}/${nextPageSuffix}`)
 }
