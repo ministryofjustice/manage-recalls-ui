a3febbac1f50667e7a28f0f1df31a681c9ede24a
Jon Wyatt <jon.wyatt@digital.justice.gov.uk>
2022-02-04 11:10:59 +0000

    Sort courts alphabetically

--- server/referenceData/courts.test.ts
+++ server/referenceData/courts.test.ts
@@ -6,16 +6,16 @@ jest.mock('../clients/manageRecallsApiClient')
 describe('Courts ref data', () => {
   ;(getCourts as jest.Mock).mockResolvedValue([
     {
-      courtId: 'ONE',
-      courtName: 'First one',
-    },
-    {
       courtId: 'TWO',
       courtName: 'Second one',
     },
+    {
+      courtId: 'ONE',
+      courtName: 'First one',
+    },
   ])
 
-  it('fetches and formats a list of courts', async () => {
+  it('fetches and formats a list of courts, sorted alphabetically', async () => {
     jest.useFakeTimers()
     const promise = courts.updateData()
     jest.advanceTimersByTime(5000)
