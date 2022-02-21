6869d867892aaee0448de5820efa97d185d2b2c2
Jon Wyatt <jon.wyatt@digital.justice.gov.uk>
2022-02-21 08:55:50 +0000

    Regenerate PACT

--- pact/getStopReasons.pact.test.ts
+++ pact/getStopReasons.pact.test.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { pactWith } from 'jest-pact'
 import { Matchers } from '@pact-foundation/pact'
-import { getPrisons } from '../server/clients/manageRecallsApiClient'
+import { getStopReasons } from '../server/clients/manageRecallsApiClient'
 import * as configModule from '../server/config'
 import getStopReasonsJson from '../fake-manage-recalls-api/stubs/__files/get-stop-reasons.json'
 import { pactGetRequest, pactJsonResponse } from './pactTestUtils'
@@ -15,10 +15,10 @@ pactWith({ consumer: 'manage-recalls-ui', provider: 'manage-recalls-api' }, prov
     test('can successfully retrieve a list of stop recall reasons', async () => {
       await provider.addInteraction({
         state: 'no state required',
-        ...pactGetRequest('a get prisons request', '/reference-data/stop-reasons'),
+        ...pactGetRequest('a get stop reasons request', '/reference-data/stop-reasons'),
         willRespondWith: pactJsonResponse(Matchers.like(getStopReasonsJson), 200),
       })
-      const actual = await getPrisons()
+      const actual = await getStopReasons()
       expect(actual).toEqual(getStopReasonsJson)
     })
   })
