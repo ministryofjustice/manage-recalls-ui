b96207727e3c9518330240cc191bb884d481df44
Jon Wyatt <jon.wyatt@digital.justice.gov.uk>
2022-02-24 17:08:20 +0000

    Add items to change history field list

--- server/controllers/changeHistory/getSingleFieldChangeHistory.test.ts
+++ server/controllers/changeHistory/getSingleFieldChangeHistory.test.ts
@@ -36,14 +36,14 @@ describe('getFieldChangeHistory', () => {
         recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         updatedByUserName: 'Maria Badger',
         updatedDateTime: '2022-01-04T13:17:34.000Z',
-        updatedValue: '2022-01-04T13:17:34.000Z',
+        updatedValue: '2022-01-04',
       },
       {
         auditId: 1,
         recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         updatedByUserName: 'Maria Badger',
         updatedDateTime: '2022-01-04T17:56:22.000Z',
-        updatedValue: '2022-01-03T13:17:34.000Z',
+        updatedValue: '2022-01-03',
       },
     ])
     const res = mockRes()
@@ -63,7 +63,7 @@ describe('getFieldChangeHistory', () => {
           recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
           updatedByUserName: 'Maria Badger',
           updatedDateTime: '2022-01-04T17:56:22.000Z',
-          updatedValue: '2022-01-03T13:17:34.000Z',
+          updatedValue: '2022-01-03',
         },
         {
           auditId: 0,
@@ -71,10 +71,78 @@ describe('getFieldChangeHistory', () => {
           recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
           updatedByUserName: 'Maria Badger',
           updatedDateTime: '2022-01-04T13:17:34.000Z',
-          updatedValue: '2022-01-04T13:17:34.000Z',
+          updatedValue: '2022-01-04',
         },
       ],
       label: 'Sentence expiry date',
+    })
+  })
+
+  it('formats an enum', async () => {
+    ;(getSingleFieldHistory as jest.Mock).mockResolvedValue([
+      {
+        auditId: 0,
+        recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        updatedByUserName: 'Maria Badger',
+        updatedDateTime: '2022-01-04T13:17:34.000Z',
+        updatedValue: 'NO_FIXED_ABODE',
+      },
+    ])
+    const res = mockRes()
+    await getSingleFieldChangeHistory(
+      mockReq({
+        params: { recallId: '456' },
+        query: { fieldName: 'lastKnownAddressOption', fieldPath: 'lastKnownAddressOption' },
+      }),
+      res,
+      mockNext()
+    )
+    expect(res.locals.fieldHistory).toEqual({
+      items: [
+        {
+          auditId: 0,
+          recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          updatedByUserName: 'Maria Badger',
+          updatedDateTime: '2022-01-04T13:17:34.000Z',
+          updatedValue: 'NO_FIXED_ABODE',
+          formattedValue: 'No fixed abode',
+        },
+      ],
+      label: 'Has last known address',
+    })
+  })
+
+  it('formats a date-time', async () => {
+    ;(getSingleFieldHistory as jest.Mock).mockResolvedValue([
+      {
+        auditId: 0,
+        recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        updatedByUserName: 'Maria Badger',
+        updatedDateTime: '2022-01-04T13:17:34.000Z',
+        updatedValue: '2022-01-04T13:17:34.000Z',
+      },
+    ])
+    const res = mockRes()
+    await getSingleFieldChangeHistory(
+      mockReq({
+        params: { recallId: '456' },
+        query: { fieldName: 'returnedToCustodyDateTime', fieldPath: 'returnedToCustodyDateTime' },
+      }),
+      res,
+      mockNext()
+    )
+    expect(res.locals.fieldHistory).toEqual({
+      items: [
+        {
+          auditId: 0,
+          recallId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          updatedByUserName: 'Maria Badger',
+          updatedDateTime: '2022-01-04T13:17:34.000Z',
+          updatedValue: '2022-01-04T13:17:34.000Z',
+          formattedValue: '4 January 2022 at 13:17',
+        },
+      ],
+      label: 'RTC date and time',
     })
   })
 })
