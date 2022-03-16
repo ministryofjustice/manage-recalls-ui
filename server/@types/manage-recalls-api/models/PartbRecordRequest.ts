2c1efc2c2ed00bfdf803a5332a0f5c23042a3637
Jon Wyatt <jon.wyatt@digital.justice.gov.uk>
2022-03-16 13:15:11 +0000

    Types / PACT

---
+++ server/@types/manage-recalls-api/models/PartbRecordRequest.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type PartBRecordRequest = {
+  details: string;
+  partBReceivedDate: string;
+  partBFileName: string;
+  partBFileContent: string;
+  emailFileName: string;
+  emailFileContent: string;
+  oasysFileName: string;
+  oasysFileContent: string;
+}
