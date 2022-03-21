3a307e8352b7738639fcea0338db76ed5c376fff
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-03-21 11:19:20 +0000

    Hack the integration tests so that they pass.
    As the integration tests are "web" code, they can't include any module
    that is "server-side" only (i.e. `prom-client` that is included in
    `restClient.js`).  This change copies in all the code needed to make the
    integration tests run, but it's a LOT of duplication.
    
    We should try and refactor `recallFieldList.ts` so that it doesn't need
    to include something that pulls in `restClient` (via depenedencies).

--- integration_tests/pages/uploadDocuments.js
+++ integration_tests/pages/uploadDocuments.js
@@ -2,6 +2,7 @@ import { documentCategories } from '../../server/controllers/documents/documentC
 
 const page = require('./page')
 
+// FIXME: copied from 'server/controllers/changeHistory/recallFieldList.ts' - rework so we can re-use that code without including restClient
 const requiredDocsList = () => documentCategories.filter(doc => doc.type === 'document' && doc.required)
 
 const uploadDocumentsPage = ({ recallId } = {}) =>
