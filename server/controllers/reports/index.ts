85ff0bdbfb92e4be821ec4dba5beabc3c387bfe4
andrew.newton <andrew.newton@digital.justice.gov.uk>
2022-05-04 09:47:24 +0100

    PUD-1714  Add reports & statistics links to home screen

--- server/controllers/reports/index.ts
+++ server/controllers/reports/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 
 export const reportsView = async (req: Request, res: Response): Promise<Response | void> => {
+  res.locals.isReportingPage = true
   res.render('pages/reports')
 }
